74e4511b29ef2c2d8160dc45b520b8e0ce5bcd47
iFreeDomain <95571453+iFreeDomain@users.noreply.github.com>
2022-05-12 20:32:13 +0800

    Update and rename config.php.default to config.php

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['db_host'] = "sql6.freesqldatabase.com"; // Database host
+$config['db_user'] = "sql6491278"; // Database user
+$config['db_pass'] = "6IebBW9dyx"; // Database password
+$config['db_name'] = "sql6491278"; // Database name
+
+?>
